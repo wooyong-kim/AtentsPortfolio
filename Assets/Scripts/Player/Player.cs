8ffe1f97d97471ef51fc105c3294c7debc655f5b
Xenexix <>
2022-12-28 12:34:50 +0900

    22/12/28
    계단 경사면 오르는 코드 작성 중

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -18,11 +18,17 @@ public class Player : CharacterProperty, IBattle
 
     public float moveSpeed = 10.0f;
     bool IsComboable = false;
-    bool Stairs = false;
     int ClickCount = 0;
 
     float gravity = 2.5f; // 중력 변수
     float yVelocity = 0; // 수직 속력 변수
+
+    public Transform myKnee;
+    public Transform myFoot;
+    RaycastHit slopHit;
+    public float maxSlopeAngle = 30.0f;
+
+    public Transform raycast;
 
     public void OnDamage(float dmg)
     {
@@ -59,8 +65,21 @@ public class Player : CharacterProperty, IBattle
     }
     private void FixedUpdate()
     {
-        myRigid.velocity = Vector3.zero; // 질량 관성 무시
-        // WallCrash();
+        //myRigid.velocity = Vector3.zero; // 질량 관성 무시
+    }
+    Vector3 AdjustDirectionToSlope(Vector3 direction)
+    {
+        return Vector3.ProjectOnPlane(direction, slopHit.normal).normalized;
+    }
+
+    float CalculateNextFrameGroundAngle(float moveSpeed) // 다음 프레임의 지면 각도
+    {
+        var nextFramePlayerPosition = raycast.position + desireDir * moveSpeed * Time.deltaTime;
+        if (Physics.Raycast(nextFramePlayerPosition, Vector3.down, out RaycastHit hitInfo, 2.0f, LayerMask.GetMask("Ground")))
+        {
+            return Vector3.Angle(Vector3.up, hitInfo.normal);
+        }
+        return 0.0f;
     }
 
     // Update is called once per frame
@@ -69,8 +88,7 @@ public class Player : CharacterProperty, IBattle
         if (IsLive)
         {
             Move();
-            StairsMove();
-            Gravity();
+            Gravity(); // 중력
             Attack();
             Shield();
             Sit();
@@ -88,8 +106,16 @@ public class Player : CharacterProperty, IBattle
         desireDir.x = Input.GetAxisRaw("Horizontal");
         desireDir.z = Input.GetAxisRaw("Vertical");
 
-        curDir.x = Mathf.Lerp(curDir.x, desireDir.x, Time.deltaTime * moveSpeed);
-        curDir.z = Mathf.Lerp(curDir.z, desireDir.z, Time.deltaTime * moveSpeed);
+
+        if(CalculateNextFrameGroundAngle(moveSpeed) < maxSlopeAngle)
+        {
+            curDir.x = Mathf.Lerp(curDir.x, desireDir.x, Time.deltaTime * moveSpeed);
+            curDir.z = Mathf.Lerp(curDir.z, desireDir.z, Time.deltaTime * moveSpeed);
+        }
+        else
+        {
+            curDir = Vector3.zero;
+        }
 
         myAnim.SetFloat("x", curDir.x);
         myAnim.SetFloat("z", curDir.z);
@@ -105,17 +131,58 @@ public class Player : CharacterProperty, IBattle
         {
             myAnim.SetBool("IsMoving", false);
         }
-        // myRigid.AddForce(curDir * moveSpeed);
+
+        bool isOnSlope = IsOnSlope();
+        if (isOnSlope)
+        {
+            // yVelocity = AdjustDirectionToSlope(desireDir);
+            gravity = 0.0f;
+        }
     }
 
-    void StairsMove()
+
+    bool PlayerStairs(Vector3 dir)
     {
-        if(Stairs && transform.position.y > posY)
+        if (Physics.Raycast(myFoot.position, dir, 0.33f, LayerMask.GetMask("Wall"))
+            || Physics.Raycast(myKnee.position, dir, 0.33f, LayerMask.GetMask("Wall")))
         {
-            transform.position += new Vector3(0.0f, 0.3f, 0.0f);
-            Debug.Log("up");
+            return true;
         }
-        posY = transform.position.y;
+        return false;
+    }
+
+    bool StairsCrash()
+    {
+        if (PlayerStairs(transform.right) || PlayerStairs(transform.forward) || PlayerStairs(-transform.right) || PlayerStairs(-transform.forward))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool PlayerWall(Vector3 dir)
+    {
+        return Physics.Raycast(transform.position, dir, 0.33f, LayerMask.GetMask("Wall"));
+    }
+
+    bool WallCrash()
+    {
+        if (PlayerWall(transform.right) || PlayerWall(transform.forward) || PlayerWall(-transform.right) || PlayerWall(-transform.forward))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    bool IsOnSlope() // Player 바닥의 경사 확인
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        if (Physics.Raycast(ray, out slopHit, 2.0f, LayerMask.GetMask("Ground")))
+        {
+            var anlge = Vector3.Angle(Vector3.up, slopHit.normal);
+            return anlge != 0.0f && anlge < maxSlopeAngle;
+        }
+        return false;
     }
 
     void Gravity() // 중력
@@ -123,17 +190,6 @@ public class Player : CharacterProperty, IBattle
         yVelocity += gravity * Time.deltaTime;
 
         myRigid.AddForce(Vector3.down * yVelocity, ForceMode.VelocityChange);
-    }
-    bool PlayerWall(Vector3 dir)
-    {
-        return Physics.Raycast(transform.position, dir, 0.33f, LayerMask.GetMask("Wall"));
-    }
-    void WallCrash()
-    {
-        if(PlayerWall(transform.right) || PlayerWall(transform.forward) || PlayerWall(-transform.right) || PlayerWall(-transform.forward))
-        {
-            AnimatorRootMotionMove();
-        }
     }
 
     void AnimatorRootMotionMove() // 벽에 충돌시 켜짐
@@ -254,21 +310,6 @@ public class Player : CharacterProperty, IBattle
             {
                 myAnim.SetTrigger("ComboStop");
             }
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Stairs"))
-        {
-            Stairs = true;
-        }
-    }
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Stairs"))
-        {
-            Stairs = false;
         }
     }
 }
--- Assets/Scripts/Player/RootMotion.cs
+++ Assets/Scripts/Player/RootMotion.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class RootMotion : MonoBehaviour
 {
     Vector3 moveDelta = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void FixedUpdate()
     {
